6af6cae46f1ce0382f235de281d0d340020096bf
dev-paulocesar1 <paulo.programacao3625@gmail.com>
2021-08-04 17:28:39 -0300

    refactor: include Board component in app file

--- src/app.js
+++ src/app.js
@@ -3,11 +3,13 @@ import React from 'react';
 import GlobalStyle from './styles/global';
 
 import Header from './components/header';
+import Board from './components/board';
 
 function App() {
   return (
     <>
       <Header />
+      <Board />
 
       <GlobalStyle />
     </>
